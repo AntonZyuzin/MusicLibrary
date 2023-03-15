11139c483a3a820513afa8c2b2db8f4ad69ee825
AntonZyuzin <71172648+AntonZyuzin@users.noreply.github.com>
2023-03-15 18:00:30 +0300

    Add files via upload

---
+++ MusicLibrary/wwwroot/js/addForm.js
@@ -0,0 +1,19 @@
+﻿const indexBody = document.getElementById('index_body')
+
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById('close_pop_up');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click', function (e) {
+    e.preventDefault();
+    popUp.classList.add('active');
+    indexBody.classList.add('active');
+    return false;
+})
+
+closePopUp.addEventListener('click', function (e) {
+    e.preventDefault();
+    popUp.classList.remove('active');
+    indexBody.classList.remove('active');
+    return false;
+})
---
+++ MusicLibrary/wwwroot/js/genrePage.js
@@ -0,0 +1,13 @@
+﻿const openGenrePop = document.getElementById('open_genre')
+const closeGenrePage = document.getElementById('close_genre');
+const genrePage = document.getElementById('genre_page')
+
+openGenrePop.addEventListener('click', function (x) {
+    x.preventDefault();
+    genrePage.classList.add('active');
+})
+
+closeGenrePage.addEventListener('click', function (x) {
+    x.preventDefault();
+    genrePage.classList.remove('active');
+})
